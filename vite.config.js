2509e47393dadb3110e87b53d3f569d41adbf197
Jr bishop <jrbishop1616@gmail.comm>
2024-06-08 00:30:28 -0400

    thirdfix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://king1661.github.io/The_amazing_portfolio/",
+  base: "hhttps://king1661.github.io/Portfolio/",
   plugins: [react()],
 })
